1fec78cb3dfa94c1ffbd026310a845e6749b593e
vahid <vahid.sd.programmer@gmail.com>
2025-10-26 16:54:43 +0330

    search input

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,6 +1,14 @@
-const GlobalSearch = ()=>{
-    return<>
+import Image from "next/image";
+import search from '@/assets/icons/search.svg';
+import { Input } from "@/components/ui/input";
 
-    </>
+
+const GlobalSearch = ()=>{
+    return(<div className={'relative w-full max-w-[600px] max-lg:hidden'}>
+        <div className={'background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4'}>
+            <Image className={'cursor-pointer'} src={search} alt={'search'} width={24} height={24}/>
+            <Input className={'placeholder:text-light-400 dark:placeholder:text-light-300 border-none shadow-none no-focus paragraph-regular outline-none background-light800_darkgradient dark:text-light-900'} type={'text'} placeholder={'search...'}/>
+        </div>
+    </div>)
 }
 export default GlobalSearch;
